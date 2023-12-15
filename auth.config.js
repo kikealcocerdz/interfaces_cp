c3bf403ecf28e9b44449272e1b8c342c9c30a454
I-Herman <iherman@hawaii.edu>
2023-12-16 00:39:10 +0100

    hopefully all of the login and registration stuff is finished

--- auth.config.js
+++ auth.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'auth-astro'
 import Credentials from "@auth/core/providers/credentials";
 import fs from 'node:fs/promises';
 
+const userDirPath = import.meta.env.USER_DATA;
 export default defineConfig({
     providers: [
         Credentials({
@@ -13,14 +14,14 @@ export default defineConfig({
             async authorize(credentials, req) {
                 let userString;
                 try {
-                    userString = await fs.readFile(`${import.meta.env.USER_DATA}.${credentials.username}.json`)
-                } catch {
-                    return null;
+                    userString = await fs.readFile(`${userDirPath}.${credentials.username}.json`)
+                } catch (e) {
+                    throw new Error("User Not Found");
                 }
                 const user = JSON.parse(userString);
 
-                return (user.password === credentials.password) * user; // Return user object if passwords match
-            }
+                return user.password === credentials.password ? user : null; // Return user object if passwords match
+            },
         })
-    ]
-})
+    ],
+});
--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,8 @@
-module.exports = {
-  plugins: {
+export const plugins = {
     'postcss-import': {},
     'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
     'postcss-preset-env': {
-      features: { 'nesting-rules': false },
+        features: {'nesting-rules': false},
     },
-  }
-}
+};
