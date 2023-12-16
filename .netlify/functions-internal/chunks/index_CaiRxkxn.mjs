abcc71387a65cf56637801f66597bfcae14299d0
kikealcocerdz <enrique.alcocer1203@gmail.com>
2023-12-16 14:51:18 +0100

    new build

--- .netlify/functions-internal/chunks/index_CaiRxkxn.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/index_XP44aM0s.mjs');
-
-export { page };
--- .netlify/functions-internal/chunks/registrar_O_EbAKr3.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/registrar_f-Le_VDm.mjs');
-
-export { page };
